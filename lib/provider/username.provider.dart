94c68c33592572e079cd597ef3fe0cd5210dc935
SilverStardust <Yuristeelf@gmail.com>
2023-02-08 16:55:15 +0100

    username provider

---
+++ lib/provider/username.provider.dart
@@ -0,0 +1,5 @@
+// Doir aller chercher le username dans les shared prefs
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+final usernameProvider = FutureProvider<String>((ref) async{
+
+});
