5bbcb4d27d5351d23b66e870ec5165c2b7153f9a
vandaele.joshua <folfyblu@gmail.com>
2023-03-01 14:15:42 +0100

    Login Page: Reposition elements

--- lib/route/login/login.route.dart
+++ lib/route/login/login.route.dart
@@ -8,16 +8,26 @@ class LoginRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-          Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
-        Spacer(),
-        LittlewordsLogo(),
-        Spacer(),
-        TextField(
-          decoration: InputDecoration(filled: true),
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Spacer(),
+        const LittlewordsLogo(),
+        const Spacer(),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: TextField(
+            decoration: InputDecoration(filled: true),
+          ),
         ),
-        ElevatedButton(
-          onPressed: null,
-          child: Text("Enregistrer nom"),
+
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 32),
+          child: ElevatedButton(
+            onPressed: null,
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(48),
+            ),
+            child: const Text("Enregistrer nom"),
+          ),
         )
       ]),
     );
