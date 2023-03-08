2fda2e885318758c76838767100d2b6b15322cf1
SilverStardust <Yuristeelf@gmail.com>
2023-03-08 13:34:47 +0100

    start add db

--- lib/db/db.helper.dart
+++ lib/db/db.helper.dart
@@ -1,3 +1,4 @@
+import 'package:littlewords/beans/dto/word.dto.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DbHelper{
@@ -14,9 +15,41 @@ class DbHelper{
     _db = database;
   }
 
+  static const String tableName = "words";
+
+  static const String createTable = '''
+  CREATE TABLE IF NOT EXISTS $tableName (
+    vid INTEGER PRIMARY KEY NOT NULL,
+    username VARCHAR NOT NULL
+  )
+  ''';
+
+  static const String dropTable = '''
+   DROP TABLE IF EXISTS $tableName
+  ''';
+
   static  _onCreate(Database db, int version) {
+    db.execute(createTable);
   }
 
   static _onUpgrade(Database db, int oldVersion, int newVersion) {
+    db.execute(dropTable);
+
+    _onCreate(db, newVersion);
+  }
+
+  Future<List<WordDTO>> findAll() async{
+    final List<Map<String, Object?>> resultSet = await _db!.query(tableName);
+    if(resultSet.isEmpty){
+      return [];
+    }
+
+    final List<WordDTO> words = [];
+    for (var map in resultSet){
+      var wordDTO = WordDTO.fromJson(map);
+      words.add(wordDTO);
+    }
+
+    return words;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'littlewordsapp.dart';
+import 'package:littlewords/db/db.helper.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const LittleWordsApp());
 }
